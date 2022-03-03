9784d512e6eeec2e8fac0857a1a12ec25b97ff7e
tagzara <tagzara82@gmail.com>
2022-03-03 14:15:52 +0200

    updated comments model

--- services/products.js
+++ services/products.js
@@ -25,7 +25,10 @@ async function getAll(query) {
 async function getById(id) {
     const cube = await Cube
         .findById(id)
-        .populate('comments')
+        .populate({
+            path: 'comments',
+            populate: { path: 'author' }
+        })
         .populate('accessories')
         .populate('author')
         .lean();
@@ -36,7 +39,7 @@ async function getById(id) {
             description: cube.description,
             imageUrl: cube.imageUrl,
             difficulty: cube.difficulty,
-            comments: cube.comments,
+            comments: cube.comments.map(c => ({ content: c.content, author: c.author.username })),
             accessories: cube.accessories,
             author: cube.author?.username,
             authorId: cube.author?._id
